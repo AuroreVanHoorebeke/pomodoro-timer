25624ecca1255904bc4662023e61a12912837f12
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2020-12-16 14:37:48 +0100

    fixed stop button, resets timer and added booleans to enable/disable start button

--- pomodoro.js
+++ pomodoro.js
@@ -4,14 +4,20 @@ const timer = document.querySelector(".timer")
 const startButton = document.querySelector(".startButton");
 const pauseButton = document.querySelector(".pauseButton");const stopButton = document.querySelector(".stopButton");
 
-let sec = 60;
+let userSecChoice=60;
+
+let sec = userSecChoice;
 let min = Math.floor((sec/60));
 let displaySec = sec%60;
 
+let start = true;
+
 timer.innerHTML = `${min}:${displaySec}`;
-//timer.innerHTML = `${min}:${sec}`
 
 function startTimer() {
+    if (start == true) {
+        start = false;
+        console.log("start is " + start);
 
     function count() {
         min = Math.floor((sec/60));
@@ -27,21 +33,34 @@ function startTimer() {
         }
 
         timer.innerHTML = `${min}:${displaySec}`;
-
     }
 
     interval = setInterval(count, 250);
+    }
 }
 
 function pauseTimer() {
     clearInterval(interval);
+    if(start == false) {
+        start = true;
+        console.log("start is "+ start);
+    }
 }
 function stopTimer() {
+    if(start == false) {
+        start = true;
+    }
+    console.log("start is "+ start);
     clearInterval(interval);
+
+    min = Math.floor((userSecChoice/60));
+    displaySec = userSecChoice%60;
     timer.innerHTML = `${min}:${displaySec}`;
-    startButton.removeEventListener("click", startTimer);
+
+    sec = userSecChoice;
 }
 
 startButton.addEventListener("click", startTimer);
 pauseButton.addEventListener("click", pauseTimer);
 stopButton.addEventListener("click", stopTimer);
+
